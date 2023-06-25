5df22f88b0c5470321f70af28ad1c1ffc4a3d6f3
Varphone Wong <varphone@qq.com>
2023-06-25 14:17:37 +0800

    PatternBrush: 优化代码
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/PatternBrush.cpp
+++ src/PatternBrush.cpp
@@ -4,33 +4,21 @@
 #include <QColor>
 #include <QGradient>
 #include <QImage>
+#include <QJSEngine>
 #include <QPainter>
 #include <QPixmap>
+#include <QRandomGenerator>
 #include <QString>
 #include <QtQml>
-#include <qcolor.h>
-#include <qrandom.h>
 
 PatternBrush::PatternBrush(QObject* parent) : QObject(parent)
 {
-    mImage = QImage(100, 100, QImage::Format_ARGB32);
+    mImage = QImage(400, 400, QImage::Format_ARGB32);
     mImage.fill(0xff00ffff);
     mBrush.setTextureImage(mImage);
+    // For testing, we update the brush every 15ms
+    connect(&mTimer, &QTimer::timeout, this, &PatternBrush::updatePattern);
     mTimer.setInterval(15);
-    connect(&mTimer, &QTimer::timeout, this, [this]() {
-        auto r = QRandomGenerator::global()->bounded(255);
-        auto g = QRandomGenerator::global()->bounded(255);
-        auto b = QRandomGenerator::global()->bounded(255);
-        auto bgColor = QColor(r, g, b);
-        auto fgColor = QColor(255 - r, 255 - g, 255 - b);
-        mImage.fill(bgColor);
-        QPainter p(&mImage);
-        p.setPen(fgColor);
-        p.drawLine(0, 0, mSize.width(), mSize.height());
-        p.drawLine(0, mSize.height(), mSize.width(), 0);
-        mBrush.setTextureImage(mImage);
-        emit brushChanged();
-    });
     mTimer.start();
 }
 
@@ -38,18 +26,10 @@ PatternBrush::~PatternBrush()
 {
 }
 
-const QBrush& PatternBrush::brush() const
+QBrush PatternBrush::brush() const
 {
     return mBrush;
 }
-
-// void PatternBrush::setBrush(const QBrush& brush)
-// {
-//     if (mBrush != brush) {
-//         mBrush = brush;
-//         emit brushChanged();
-//     }
-// }
 
 QSize PatternBrush::size() const
 {
@@ -69,4 +49,36 @@ void PatternBrush::registerMetaTypes(const char* uri)
     qRegisterMetaType<PatternBrush>();
     qRegisterMetaType<PatternBrush>("PatternBrush&");
     qmlRegisterType<PatternBrush>(uri, 1, 0, "PatternBrush");
+}
+
+// void PatternBrush::collectGarbage()
+// {
+//     auto engine = qjsEngine(this);
+//     if (engine) {
+//         engine->collectGarbage();
+//     }
+// }
+
+QBrush PatternBrush::dummyBrush() const
+{
+    return QBrush{};
+}
+
+void PatternBrush::updatePattern()
+{
+    auto r = QRandomGenerator::global()->bounded(255);
+    auto g = QRandomGenerator::global()->bounded(255);
+    auto b = QRandomGenerator::global()->bounded(255);
+    auto bgColor = QColor(r, g, b);
+    auto fgColor = QColor(255 - r, 255 - g, 255 - b);
+    mImage.fill(bgColor);
+    {
+        QPainter p(&mImage);
+        p.setPen(QPen(fgColor, 2));
+        // 画一个叉型图案
+        p.drawLine(0, 0, mSize.width(), mSize.height());
+        p.drawLine(0, mSize.height(), mSize.width(), 0);
+    }
+    mBrush.setTextureImage(mImage);
+    emit brushChanged(mBrush);
 }
--- src/PatternBrush.h
+++ src/PatternBrush.h
@@ -10,31 +10,70 @@
 #include <QString>
 #include <QtQml>
 
+// 一个代表图案画刷的类型。
+//
+// 该类型的实例可以在 QML 中使用。
+// 该类型的实例可以在 C++ 中使用。
 class PatternBrush : public QObject {
     Q_OBJECT
-    Q_PROPERTY(const QBrush& brush READ brush NOTIFY brushChanged)
+    /// 一个代表图案画刷的属性。
+    Q_PROPERTY(QBrush brush READ brush NOTIFY brushChanged)
+    /// 一个代表图案大小的属性。
     Q_PROPERTY(QSize size READ size WRITE setSize NOTIFY sizeChanged)
     QML_ELEMENT
 public:
+    /// 构造函数。
+    /// @param parent 父对象。
     explicit PatternBrush(QObject* parent = nullptr);
+
+    /// 析构函数。
     virtual ~PatternBrush();
 
-    const QBrush& brush() const;
-    // void setBrush(const QBrush& brush);
+    /// 返回图案画刷。
+    QBrush brush() const;
 
+    /// 返回图案大小。
     QSize size() const;
+
+    /// 设置图案大小。
+    /// @param size 图案大小。
+    /// @note 该函数会发出 sizeChanged 信号。
     void setSize(const QSize& size);
 
+    /// 注册该类型的元类型。
+    /// @param uri 该类型的 URI。
     static void registerMetaTypes(const char* uri);
 
+public slots:
+    // void collectGarbage();
+
+    /// 返回一个空的图案画刷。
+    QBrush dummyBrush() const;
+
 signals:
-    void brushChanged();
+    /// 该信号在图案画刷发生变化时发出。
+    /// @param brush 新的图案画刷。
+    /// @note 该信号对应 brush 属性。
+    void brushChanged(const QBrush& brush);
+
+    /// 该信号在图案大小发生变化时发出。
+    /// @param size 新的图案大小。
+    /// @note 该信号对应 size 属性。
+    /// @note 该信号在 setSize() 函数中发出。
     void sizeChanged(const QSize& size);
 
 private:
+    /// 更新图案。
+    void updatePattern();
+
+private:
+    /// 图案大小。
     QSize mSize{20, 20};
+    /// 图案画刷。
     QBrush mBrush{};
+    /// 图案图像。
     QImage mImage{};
+    /// 定时器（用于生成测试图案）。
     QTimer mTimer{};
 };
 
